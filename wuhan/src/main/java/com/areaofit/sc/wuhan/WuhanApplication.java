b109d6c92c1d0e0b7ce49d7b724e5687e0c6a623
Areaofit <areaofit@qq.com>
2019-06-26 22:40:01 +0800

    sc project

---
+++ wuhan/src/main/java/com/areaofit/sc/wuhan/WuhanApplication.java
@@ -0,0 +1,13 @@
+package com.areaofit.sc.wuhan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WuhanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WuhanApplication.class, args);
+    }
+
+}
